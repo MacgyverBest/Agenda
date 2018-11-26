e734f1cddd9559591b8d7140c57d655929b578ed
MacgyverBest <45299348+MacgyverBest@users.noreply.github.com>
2018-11-26 17:11:15 -0200

    Update todo_e6.js

--- todo_e6.js
+++ todo_e6.js
@@ -7,39 +7,47 @@ class Todo extends Store {
     renderItem(item) {
 
         let template = `
-           <div class="card" style="width: 800px; height: 220px; margin-top: 15px">
-            <div class="card-content">
-              <div class="media">
-                <div class="media-left">
-                  <figure class="image is-48x48">
-                    <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                  </figure>
+           <div class="columns">
+        <div class="column is-6">
+            <div class="card events-card">
+                <header class="card-header">
+                    <p class="card-header-title">
+                        CONTATO
+                    </p>
+                    
+                </header>
+                <div class="card-table">
+                    <div class="content">
+                        <table class="table is-fullwidth is-striped">
+                            <tbody>
+                                   <tr>                 
+                                    <td width="5%"><i class="fa fa-bell-o"></i></td>
+                                    <td><input id="itemValue" class="input enter" type="text" disabled value="${item.nome}">
+                                      <input id="itemTelefone" class="input enter" type="text" disabled value="${item.telefone}">
+                                      <input id="itemCpf" class="input enter" type="text" disabled value="${item.cpf}">
+                                      <input id="itemEndereco" class="input enter" type="text" disabled value="${item.endereco}">
+                                    </td>
+                                    <td><a class="button is-info editar">
+                                      <span class="icon is-small">
+                                          <i class="fas fa-edit"></i>
+                                      </span>
+                                  </a>
+                                  <a style="margin-top:10px" class="button is-danger deletar">
+                                    <span class="icon is-small">
+                                        <i class="fas fa-times"></i>
+                                    </span>
+                                </a></td>
+                                </tr>
+                               
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div class="media-content" >
-             <p class="title "><input style="width: 600px; height: 30px" id="itemValue" class="input enter" size="15" type="text" disabled value="${item.nome}"></p>
-              <p class="subtitle "><input style="width: 600px; height: 30px" id="itemTelefone" class="input enter" type="text" disabled value="${item.telefone}"></p>
-               <p class="subtitle "><input style="width: 600px; height: 30px" id="itemCpf" class="input enter" type="text" disabled value="${item.cpf}"></p>
-               <p class="subtitle "><input style="width: 600px; height: 30px" id="itemEndereco" class="input enter" type="text" disabled value="${item.endereco}"></p>
-                </div>
-              
-              
-             <p class="control" style="margin-left:10px"  >
-                    <a class="button is-info editar">
-                        <span class="icon is-small">
-                            <i class="fas fa-edit"></i>
-                        </span>
-                    </a>
-                </p>
-                <p class="control" style="margin-left:10px">
-                    <a class="button is-danger deletar">
-                        <span class="icon is-small">
-                            <i class="fas fa-times"></i>
-                        </span>
-                    </a>
-                </p>
-          </div>
-                 
-            </div>`
+               
+            </div>
+        </div>
+    
+    </div>`
 
         const itemHTML = document.createRange().createContextualFragment(template)
 
